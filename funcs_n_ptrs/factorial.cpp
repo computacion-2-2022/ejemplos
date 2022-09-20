6d369336cd5c39a159b8646c817bafcf351dd475
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-09-20 11:19:47 +0200

    Add iterative solution for the factorial
    We actually like it better...

--- funcs_n_ptrs/factorial.cpp
+++ funcs_n_ptrs/factorial.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 
-long int factorial(long int);
+long int factorialRecursive(long int);
+long int factorialIterative(long int);
 
 int main() {
     long int n = 0;
     std::cout << "Number to compute the factorial for: ";
     std::cin >> n;
 
-    std::cout << n << "! = " << factorial(n) << std::endl;
+    std::cout << n << "! = " << factorialRecursive(n) << " == " << factorialIterative(n) << std::endl;
 
     return 0;
 }
 
-long int factorial(long int n) {
+long int factorialRecursive(long int n) {
     if (!n)
         return 1;
-    return n * factorial(n - 1);
+    return n * factorialRecursive(n - 1);
+}
+
+
+long int factorialIterative(long int n) {
+    long int fact = 1;
+
+    for (int i = n; i > 1; i--)
+        fact *= i;
+
+    return fact;
 }
